7c0e453397d62edb059bbc2d23c4f9dc7aa55cf5
Mahar Shahzaib <shahzaibniazdev.com>
2026-08-08 04:54:12 +0500

    Strech goal

--- DayOne/task/fundamentals.js
+++ DayOne/task/fundamentals.js
@@ -49,7 +49,6 @@ Answere: because there was many issues with var .
 var redeclaration may cause the variable collion or overwrite the veriable .
 var is not block scope its value is accessible even outside the scope .
 it creates the hoiting confusion because in creation of execution context var is initialized with undefined 
-
 so that makes confusion even the varibale exists it show undefined ;
 so let and const solves these issue => they can't be redeclared and both are function scope and block scope and solve the hoiting confusion both stored in memory uninitialized .
 
@@ -57,3 +56,24 @@ so let and const solves these issue => they can't be redeclared and both are fun
 
 
 */
+
+//  Write a typeOf(value) function that correctly identifies arrays and null (typeof alone fails on both) without using any library
+
+function typeOf(value) {
+    // to conver the type of null from object to null
+    if (value === null) {
+        return "null";
+    }
+    // to convert type of array to array
+    if (Array.isArray(value)) {
+        return "array";
+    }
+
+    return typeof value;
+}
+
+console.log(typeOf(null));       // null
+console.log(typeOf([]));         // array
+console.log(typeOf("hello"));    // string
+console.log(typeOf(25));         // number
+console.log(typeOf(true));       // boolean
